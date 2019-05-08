824a430edcb07113a3e9517b05d3e459c811c423
flymans <746590@mail.ru>
2019-05-08 21:55:40 +0300

    enable CORS

--- src/app.js
+++ src/app.js
@@ -14,6 +14,13 @@ import Book from "./models/book";
 dotenv.config();
 
 const app = express();
+//enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //API
 app.get("/books", (req, res) => {
   const { name, author, store, page = 1, perPage = 50 } = req.query;
